8adf47063369b8dcd21aadfe90226eff551bcf7f
Shibarjun <shibarjunmandal@gmail.com>
2023-07-26 12:12:21 +0100

    got the z plane value, which will be used for displaying the bacterial region

--- src/findaureus/_widget.py
+++ src/findaureus/_widget.py
@@ -21,71 +21,32 @@ class Find_Bacteria(QWidget):
         super().__init__()
         self.viewer = napari_viewer
         self.init_ui()
-        self.viewer.layers.selection.events.active.connect(self.on_layer_selection_change)
-        self.viewer.dims.events.current_step.connect(self.on_active_layer_change)
+        
 
     def init_ui(self):
         layout = QVBoxLayout()
 
-        self.label1 = QLabel("Hello, welcome to the widget Findaureus \n Please select the channel!")
-        layout.addWidget(self.label1)
+        self.welcome_label = QLabel("Hello, welcome to the widget Findaureus \n Please select the channel!")
+        layout.addWidget(self.welcome_label)
 
         button = QPushButton("Find bacteria!")
         button.clicked.connect(self.FindBacteria)
         layout.addWidget(button)
-        self.label2 = QLabel("Channel selected: ")
-        layout.addWidget(self.label2)
         
-        self.label3 = QLabel("No of channel with bacteria: \nNo of channel without bacteria:")
-        layout.addWidget(self.label3)
+        self.Channel_label= QLabel("Channel selected: ")
+        layout.addWidget(self.Channel_label)
         
-        self.additional_text_label = QLabel("Additional Text:")
-        layout.addWidget(self.additional_text_label)
+        self.bacteria_info_label1 = QLabel("")
+        layout.addWidget(self.bacteria_info_label1)
+        
+        self.bacteria_info_label2 = QLabel("")
+        layout.addWidget(self.bacteria_info_label2)
 
         self.setLayout(layout)
         
-    #     self.update_layer_combo()
-    
-    # def get_layer_names(self):
-    #     # Function to get a list of layer names in a thread-safe manner.
-    #     return [layer.name for layer in self.viewer.layers]
-    
-    # def update_layer_combo(self):
-    #     # Update the combobox with the available layer names.
-    #     layer_names = self.get_layer_names()
-    #     self.layer_combo.clear()
-    #     self.layer_combo.addItems(layer_names)
-
-    # def on_button_click(self):
-    #     # Example action to perform when the button is clicked
-    #     # layout = QVBoxLayout()
-    #     current_layer = self.viewer.layers.selection.active
         
-    #     if current_layer is not None:
-    #         self.label.setText(f"Channel selected: {current_layer.name}")
-    #         print(f"Active layer name: {current_layer.name}")
-    #     else:
-    #         print("No active layer selected.")
-
-
-
-# class ExampleQWidget(QWidget):
-#     # your QWidget.__init__ can optionally request the napari viewer instance
-#     # in one of two ways:
-#     # 1. use a parameter called `napari_viewer`, as done here
-#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-#     def __init__(self, napari_viewer):
-#         super().__init__()
-#         self.viewer = napari_viewer
-
-#         btn = QPushButton("Click me!")
-#         btn.clicked.connect(self._on_click)
-
-#         self.setLayout(QHBoxLayout())
-#         self.layout().addWidget(btn)
-
-#     def _on_click(self):
-#         print("napari has", len(self.viewer.layers), "layers")
+        self.viewer.layers.selection.events.active.connect(self.on_layer_selection_change)
+        
     def for_napari(image_list):
         data =np.stack(image_list)
         data = np.expand_dims(data, -1)
@@ -97,26 +58,22 @@ class Find_Bacteria(QWidget):
     def FindBacteria(self)-> "napari.types.LayerDataTuple":
         current_layer = self.viewer.layers.selection.active
         if current_layer is not None:
-        
+            
             image_list = list(current_layer.data[0,:,:,:])
             _,scalez,scalex ,scaley  = current_layer.scale 
             scalexy = (scalex,scaley)
             scalezxy = (scalez,scaley,scalex)
             bac_image_list,bac_image_list_mask, bac_centroid_xy_coordinates, no_bac_dict, bac_pixelwise_xy_coordinates, bacteria_area = ReadImage.FindBacteriaAndNoBacteria(image_list, scalexy)
-            # z_value, x_value, y_value = ReadImage.ImageScalingXY
-            # bac_data_box = Find_Bacteria.for_napari(bac_image_list)
             bac_data_mask = Find_Bacteria.for_napari(bac_image_list_mask)
-            # current_z_plane = self.viewer.dims.point[0]
-            # bac_data = np.concatenate ((bac_data_box,bac_data_mask), axis = 2)
-            # bac_layer = (bac_data, {"scale": scalezxy,"name": (f"{img_layer.name}_Bbox", f"{img_layer.name}_mask")})
-            # bac_layer = (bac_data_mask, {"scale": scalezxy,"name": f"{current_layer.name}_Bbox", "opacity":0.5})
             print("Findbac Done")
-            self.label1.setText("Image processed and added as a new layer.")
-            self.label3.setText(f"No of channel with bacteria:.{len(bac_image_list)} \nNo of channel without bacteria: {len(no_bac_dict)} ")
+            self.welcome_label.setText("Image processed and added as a new layer.")
+            self.bacteria_info_label1.setText(f"No. of Channel with Bacteria:.{len(bac_image_list)} \nNo. of Channel without Bacteria: {len(no_bac_dict)} ")
             self.viewer.add_image(bac_data_mask, name=f"{current_layer.name}_Bacteria mask", scale= scalezxy, opacity=0.5, colormap='red')
+            self.viewer.dims.events.current_step.connect(self.on_active_layer_change)
+            current_z_plane_bac = Find_Bacteria.on_active_layer_change(self)
+            
         else:
-            self.label1.setText("No active layer selected.")
-        # return [bac_layer]
+            self.welcome_label.setText("No active layer selected.")
         
     def for_raw_layer(active_layer):
         
@@ -136,28 +93,13 @@ class Find_Bacteria(QWidget):
             # Update the text with information about the selected layer
             layer_name, layer_height_um, layer_height_px, layer_width_um, layer_width_px, depth_um,scalex = Find_Bacteria.for_raw_layer(active_layer)
             
-            self.label2.setText(f"Channel selected: {layer_name} \nImage height: {layer_height_um} microns ({layer_height_px}) \nImage width: {layer_width_um} microns ({layer_width_px}) \nImage depth: {depth_um} microns \nImage resolution: {round(1/scalex)} pixels per micron")
+            self.Channel_label.setText(f"Channel selected: {layer_name} \nImage height: {layer_height_um} microns ({layer_height_px}) \nImage width: {layer_width_um} microns ({layer_width_px}) \nImage depth: {depth_um} microns \nImage resolution: {round(1/scalex)} pixels per micron")
         except:
             
-            self.label2.setText("No active layer selected.")
+            self.Channel_label.setText("No active layer selected.")
             
-    def on_active_layer_change(self, event):
-    # Function to update the Z-plane value when the current step changes
-        active_layer = self.viewer.layers.selection.active
-        if active_layer is not None:
-            # Get the current Z-plane value from the current_step tuple
-            current_z_plane = self.viewer.dims.current_step[1]  # Index 1 corresponds to the Z-plane value
     
-            # Update the text with the current Z-plane value
-            self.additional_text_label.setText(f"Current Z-plane: {current_z_plane}")
-        else:
-            self.additional_text_label.setText("Current Z-plane: N/A")
-
-
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-# def example_function_widget(img_layer: "napari.layers.Image"):
-#     print(f"you have selected {img_layer}")
+    def on_active_layer_change(self):
+        current_z_plane = self.viewer.dims.current_step[2]  # Index 2 corresponds to the new layer Z-plane value
+        self.bacteria_info_label2.setText(f"No. of Bacterial Region: {current_z_plane}")
+        return(current_z_plane)
