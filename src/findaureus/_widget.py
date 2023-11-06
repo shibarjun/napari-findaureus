f57ecf5ee3d428c04f43c67a25b24befc161cd7a
Shibarjun <shibarjunmandal@gmail.com>
2023-11-06 10:40:44 +0100

    bak file update

--- src/findaureus/_widget.py
+++ src/findaureus/_widget.py
@@ -13,7 +13,7 @@ import napari.layers
 if TYPE_CHECKING:
     import napari
 
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QPlainTextEdit, QHBoxLayout, QTextEdit
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QPlainTextEdit, QHBoxLayout, QTextEdit, QDialog
 from qtpy.QtCore import Qt, QUrl
 from qtpy.QtGui import QFont, QPixmap, QCursor, QDesktopServices
 
@@ -200,7 +200,7 @@ class Find_Bacteria(QWidget):
         
     def open_instruction(self):
         if not self.instruction_window or not self.instruction_window.isVisible():
-            self.instruction_window = QWidget()
+            self.instruction_window = QDialog()
             self.instruction_window.setWindowTitle("Instruction")
 
             layout = QVBoxLayout()
@@ -238,5 +238,5 @@ class Find_Bacteria(QWidget):
             self.instruction_window.adjustSize()
             self.instruction_window.show()
     
-    def close_instruction_window(self, event):
+    def closeEvent(self, event):
         self.instruction_window.close()
